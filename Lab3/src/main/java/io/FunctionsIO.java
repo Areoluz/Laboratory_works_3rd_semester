d71b4e2a6bb69fc8b7f0f4b769703dd51966d403
Areolus <232242jov@gmail.com>
2024-10-31 04:09:30 +0300

    FileWriter
    ВАЖНО:
    Не работает с OneDrive'ом поэтому указан абсолютный путь на другой диск

--- Lab3/src/main/java/io/FunctionsIO.java
+++ Lab3/src/main/java/io/FunctionsIO.java
@@ -1,7 +1,20 @@
 package io;
+
+import functions.TabulatedFunction;
+
+import java.io.*;
+
 public final class FunctionsIO {
     // Приватный конструктор, чтобы запретить создание экземпляров класса
     private FunctionsIO() {
         throw new UnsupportedOperationException("This class cannot be instantiated.");
+    }
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) throws IOException {
+        PrintWriter printWriter = new PrintWriter(writer);
+        printWriter.println(function.getCount());
+        for (var point : function) {
+            printWriter.printf("%f %f%n", point.x, point.y);
+        }
+        writer.flush();
     }
 }
