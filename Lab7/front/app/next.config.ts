63662401e3ed78835a757e0c80df90d4720550fc
julia konnina <julia.konnina@gmail.com>
2024-12-25 17:11:32 +0400

    config

--- Lab7/front/app/next.config.ts
+++ Lab7/front/app/next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: "http://localhost:8080/:path*", // Это позволит правильно перенаправлять запросы
+            },
+        ];
+    },
+    skipTrailingSlashRedirect: true,
+    output: "standalone",
 };
 
 export default nextConfig;
